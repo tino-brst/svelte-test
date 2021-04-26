5ad2339a0c6fa7dbe3765de2dc78baf072fad0cd
Agustin Burset <agustin.burset@icloud.com>
2021-04-26 07:43:09 -0300

    Add shape selector, viewer setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,93 @@
 <script>
+  const Shape = {
+    box: 'box',
+    sphere: 'sphere',
+  }
+
+  const shapeOptions = [
+    { label: 'Box', value: Shape.box },
+    { label: 'Sphere', value: Shape.sphere },
+  ]
+
+  let selectedShape = Shape.box
 </script>
 
-<div />
+<div class="configurator">
+  <vctr-viewer
+    class="configurator__viewer"
+    id="vectary-viewer"
+    model="60872dd8-5cb0-4c79-9a9c-91baf9944a17"
+    mouseFollow="0.5"
+  />
+  <div class="configurator__controls">
+    <h2 class="group-title">Shape</h2>
+    {#each shapeOptions as option}
+      <label class="option" class:option_selected={selectedShape === option.value}>
+        <input
+          class="option__input"
+          type="radio"
+          bind:group={selectedShape}
+          value={option.value}
+          checked
+        />{option.label}</label
+      >
+    {/each}
+  </div>
+</div>
 
 <style>
+  .configurator {
+    background: whitesmoke;
+    height: 100%;
+  }
+
+  .configurator__viewer {
+    height: 100%;
+  }
+
+  .configurator__controls {
+    background-color: white;
+    border-radius: 8px;
+    position: fixed;
+    padding: 14px;
+    bottom: 10px;
+    left: 10px;
+    right: 10px;
+    box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.1);
+  }
+
+  .group-title {
+    margin-top: 0;
+    margin-bottom: 8px;
+  }
+
+  .option {
+    display: inline-block;
+    border-radius: 8px;
+    padding: 4px 8px;
+    user-select: none;
+    color: rgb(110, 110, 110);
+    cursor: pointer;
+    transition: all 0.15s;
+  }
+
+  .option:not(:last-child) {
+    margin-right: 4px;
+  }
+
+  .option:hover {
+    color: black;
+    background-color: whitesmoke;
+  }
+
+  .option.option_selected {
+    color: black;
+    background-color: rgb(230, 230, 230);
+    font-weight: 500;
+  }
+
+  .option__input {
+    appearance: none;
+    margin: 0;
+  }
 </style>
